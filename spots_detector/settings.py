ae3df196131e6d863e74f6c65a2e55bceee3e6aa
ArthurCisotto <arthurcisottom@gmail.com>
2023-11-23 08:25:33 -0300

    Update files vercel

--- spots_detector/settings.py
+++ spots_detector/settings.py
@@ -93,10 +93,6 @@ WSGI_APPLICATION = 'spots_detector.wsgi.app'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
 }
 
 
