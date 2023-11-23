130898746abf56670c7225b59c7c3f1f15df5699
ArthurCisotto <arthurcisottom@gmail.com>
2023-11-23 11:55:01 -0300

    a

--- spots_detector/settings.py
+++ spots_detector/settings.py
@@ -86,13 +86,17 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'spots_detector.wsgi.app'
+WSGI_APPLICATION = 'spots_detector.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
